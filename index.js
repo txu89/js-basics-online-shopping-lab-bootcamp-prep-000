71e34248411a773ac9db80d623e77652e523f709
Tom Xu <tomxu89@gmail.com>
2018-01-14 00:34:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  let itemPrice = Math.random() * 100
+  let itemPrice = Math.floor(Math.random() * 100)
   cart[item] = itemPrice
   return cart
 }
