16e70d0d6aefb277acf8e0323eb310ebeb0b46cc
Tom Xu <tomxu89@gmail.com>
2018-01-14 00:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  let cartItems = "In your cart, you have "
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  else if (cart.length === 1) {
+    cartItems += `${cart[0].
+  }
 }
 
 function total() {
